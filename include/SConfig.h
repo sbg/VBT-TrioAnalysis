739e2fe3e76b2e111b122f2d2e3b3155ce27e353
Berke Toptas <berke.toptas@sbgenomics.com>
2016-12-02 14:29:25 -0500

    Command line argument parser is implemented

--- include/SConfig.h
+++ include/SConfig.h
@@ -5,10 +5,36 @@
 #ifndef _S_CONFIG_H_
 #define _S_CONFIG_H_
 
+//Any configuration that will be used send via this config object
 struct SConfig
 {
-    //Any configuration that will be used send via this config object
+    SConfig(int argc, char** argv)
+    {
+        const char* PARAM_BASE = "-b";
+        const char* PARAM_CALLED = "-c";
+        const char* PARAM_FILTER = "-f";
+
+        int it = 1;
+        
+        while(it <= argc)
+        {
+            if(0 == strcmp(argv[it], PARAM_BASE))
+                m_pBaseVcfFileName = argv[it+1];
+            else if(0 == strcmp(argv[it], PARAM_CALLED))
+                m_pCalledVcfFileName = argv[it+1];
+            else if(0 == strcmp(argv[it], PARAM_FILTER))
+                m_pFilterName = argv[it+1];
+        
+            it += 2;
+        }
+    }
     
+    SConfig()
+    {}
+    
+    
+    //Indicates if enough parameters are received from command line
+    bool m_bIsOK;
     
     //Maximum size of the variant that will be processed by VCF comparison algorithm (Use it to eliminate SVs)
     int m_nMaxVariantSize;
@@ -22,11 +48,16 @@ struct SConfig
     //Fasta file
     const char* m_pFastaFileName;
     
+    //If filtering enabled
+    bool m_bIsFilterEnabled;
     //Filter name (Filter variants according to filter column)
     const char* m_pFilterName;
-    
+   
+    //If Quality threshold is enabled
+    bool m_bIsQualityThresholdEnabled;
     //Threshold value of the variants
     float m_fQualityThreshold;
+    
 };
 
 
