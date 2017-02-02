2067514723816f048a2cb720f61e09336fb2ef8a
Berke Toptas <berke.toptas@sbgenomics.com>
2017-02-02 15:47:26 -0500

    Mendelian Logging Added to CResultLog

--- include/CResultLog.h
+++ include/CResultLog.h
@@ -10,6 +10,7 @@
 #define _C_RESULT_LOG_H_
 
 #include <string>
+#include "Constants.h"
 
 struct SLogEntry
 {
@@ -19,6 +20,15 @@ struct SLogEntry
     int m_nHalfTpCalled;
     int m_nHalfTpBase;
     int m_nFp;
+    int m_nFn;
+};
+
+struct SMendelianLogEntry
+{
+    bool m_bIsNull = true;
+    int m_nTpCalled;
+    int m_nTpBase;
+    int m_nFP;
     int m_nFn;
 };
 
@@ -34,12 +44,23 @@ public:
     //Records the result for given chromosome
     void LogStatistic(int a_nChromosomeId, int a_nTpCalled, int a_nTpBaseline, int a_nHalfTPCalled, int a_nHalfTPBaseline, int a_nFalsePositive, int a_nFalseNegative);
     
+    //Records log entry for mendelian comparison
+    void LogMendelianStatistic(bool a_bIsFatherChild, int a_nChromosomeId, int a_nTpCalled, int a_nTpBaseline, int a_nFalsePositive, int a_nFalseNegative);
+    
     //Write the results in log.txt file
     void WriteStatistics();
     
+    //Write the results of mendelian violation check in log.txt file
+    void WriteMendelianStatistics();
+    
 private:
     
-    SLogEntry m_aResultEntries[25];
+    //Log entry array for ga4gh output [THE STANTARD OUTPUT]
+    SLogEntry m_aResultEntries[CHROMOSOME_COUNT];
+    
+    //Log entry array for Mendelian Violation mode output
+    SMendelianLogEntry m_aFatherChildLogEntries[CHROMOSOME_COUNT];
+    SMendelianLogEntry m_aMotherChildLogEntries[CHROMOSOME_COUNT];
     
     std::string m_aLogPath;
     
--- src/CResultLog.cpp
+++ src/CResultLog.cpp
@@ -28,6 +28,23 @@ void CResultLog::LogStatistic(int a_nChromosomeId, int a_nTpCalled, int a_nTpBas
     m_aResultEntries[a_nChromosomeId].m_bIsNull = false;
 }
 
+void CResultLog::LogMendelianStatistic(bool a_bIsFatherChild, int a_nChromosomeId, int a_nTpCalled, int a_nTpBaseline, int a_nFalsePositive, int a_nFalseNegative)
+{
+    SMendelianLogEntry* entry;
+    
+    if(true == a_bIsFatherChild)
+        entry = &m_aFatherChildLogEntries[a_nChromosomeId];
+    else
+        entry = &m_aMotherChildLogEntries[a_nChromosomeId];
+
+    entry->m_nTpBase = a_nTpBaseline;
+    entry->m_nTpCalled = a_nTpCalled;
+    entry->m_nFn = a_nFalseNegative;
+    entry->m_nFP = a_nFalsePositive;
+    entry->m_bIsNull = false;
+}
+
+
 //Write the results in log.txt file
 void CResultLog::WriteStatistics()
 {
@@ -106,4 +123,81 @@ void CResultLog::WriteStatistics()
     
     outputLog.close();
 }
+
+void CResultLog::WriteMendelianStatistics()
+{
+    std::ofstream outputLog;
+    outputLog.open(m_aLogPath);
+    
+    outputLog << "====== FATHER CHILD COMPARISON ======" << std::endl;
+    outputLog << "ID" << "\t" << "True-Pos-Called" << "\t\t" << "True-Pos-Baseline" << "\t" << "False-Pos" << "\t" << "False-Neg" << "\t" << "Precision" << "\t" << "Recall";
+    outputLog << "\t" << "F-measure" << std::endl;
+    
+    for(int k = 0; k < 25; k++)
+    {
+        if(m_aFatherChildLogEntries[k].m_bIsNull)
+            continue;
+        
+        if(k < 22)
+            outputLog << "Chr" << k+1;
+        else if(k == 23)
+            outputLog << "ChrX";
+        else if(k == 24)
+            outputLog << "ChrY";
+        else
+            outputLog << "ChrM";
+        
+        int TPbase = m_aFatherChildLogEntries[k].m_nTpBase;
+        int TPcalled = m_aFatherChildLogEntries[k].m_nTpCalled;
+        int FP = m_aFatherChildLogEntries[k].m_nFP;
+        int FN = m_aFatherChildLogEntries[k].m_nFn;
+        
+        outputLog << "\t" << TPcalled << "\t" << TPbase << "\t" << FP << "\t" << FN;
+        
+        double Precision = static_cast<double>(TPbase) / static_cast<double>(TPbase + FP);
+        double Recall = static_cast<double>(TPbase) / static_cast<double>(TPbase + FN);
+        double Fmeasure = Precision + Recall == 0.0 ? 0.0 : (2.0 * Precision * Recall) / (Precision + Recall);
+        
+        outputLog.precision(4);
+        outputLog << "\t" << Precision << "\t" << Recall << "\t" << Fmeasure << std::endl;
+    }
+    
+    outputLog << "====== MOTHER CHILD COMPARISON ======" << std::endl;
+    outputLog << "ID" << "\t" << "True-Pos-Called" << "\t\t" << "True-Pos-Baseline" << "\t" << "False-Pos" << "\t" << "False-Neg" << "\t" << "Precision" << "\t" << "Recall";
+    outputLog << "\t" << "F-measure" << std::endl;
+    
+    for(int k = 0; k < 25; k++)
+    {
+        if(m_aMotherChildLogEntries[k].m_bIsNull)
+            continue;
+        
+        if(k < 22)
+            outputLog << "Chr" << k+1;
+        else if(k == 23)
+            outputLog << "ChrX";
+        else if(k == 24)
+            outputLog << "ChrY";
+        else
+            outputLog << "ChrM";
+        
+        int TPbase = m_aMotherChildLogEntries[k].m_nTpBase;
+        int TPcalled = m_aMotherChildLogEntries[k].m_nTpCalled;
+        int FP = m_aMotherChildLogEntries[k].m_nFP;
+        int FN = m_aMotherChildLogEntries[k].m_nFn;
+        
+        
+        outputLog << "\t" << TPcalled << "\t" << TPbase << "\t" << FP << "\t" << FN;
+        
+        double Precision = static_cast<double>(TPbase) / static_cast<double>(TPbase + FP);
+        double Recall = static_cast<double>(TPbase) / static_cast<double>(TPbase + FN);
+        double Fmeasure = Precision + Recall == 0.0 ? 0.0 : (2.0 * Precision * Recall) / (Precision + Recall);
+        
+        outputLog.precision(4);
+        outputLog << "\t" << Precision << "\t" << Recall << "\t" << Fmeasure << std::endl;
+    }
+}
+
+
+
+
 
