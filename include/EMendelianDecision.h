65a01d6ada1be03ecfbd418f286097db79be45f0
Berke Toptas <berke.toptas@sbgenomics.com>
2017-03-13 14:35:56 -0400

    Trio Variant merger class is implemented

---
+++ include/EMendelianDecision.h
@@ -0,0 +1,19 @@
+//
+//  EMendelianDecision.h
+//  VCFComparison
+//
+//  Created by Berke.Toptas on 3/9/17.
+//  Copyright © 2017 Seven Bridges Genomics. All rights reserved.
+//
+
+#ifndef _E_MENDELIAN_DECISION_H_
+#define _E_MENDELIAN_DECISION_H_
+
+enum EMendelianDecision
+{
+    eCompliant,
+    eViolation,
+    eUnknown
+};
+
+#endif // _E_MENDELIAN_DECISION_H_
