4a56da5d1f141f19e77828240ca5ed48ea6f656f
Berke Toptas <berke.toptas@sbgenomics.com>
2017-03-29 10:43:55 -0400

    DuoComparison - mendelian related functions are removed from CResultLog

--- DuoComparison/include/CResultLog.h
+++ DuoComparison/include/CResultLog.h
@@ -23,29 +23,6 @@ struct SLogEntry
     int m_nFn;
 };
 
-struct SMendelianLogEntry
-{
-    bool m_bIsNull = true;
-    int m_nTpCalled;
-    int m_nTpBase;
-    int m_nFP;
-    int m_nFn;
-};
-
-struct SMendelianMergeLogEntry
-{
-    bool m_bIsNull = true;
-    int m_nCommonHomozygous;
-    int m_nCommonHeterozygous;
-    int m_nCommonFilteredHeterozygous;
-    int m_nFCHomozygous;
-    int m_nFCHeterozygous;
-    int m_nMCHomozygous;
-    int m_nMCHeterozygous;
-    int m_nChildUnique;
-};
-
-
 class CResultLog
 {
     
@@ -57,37 +34,15 @@ public:
     //Records the result for given chromosome
     void LogStatistic(int a_nChromosomeId, int a_nTpCalled, int a_nTpBaseline, int a_nHalfTPCalled, int a_nHalfTPBaseline, int a_nFalsePositive, int a_nFalseNegative);
     
-    //Records log entry for mendelian comparison
-    void LogMendelianStatistic(bool a_bIsFatherChild, int a_nChromosomeId, int a_nTpCalled, int a_nTpBaseline, int a_nFalsePositive, int a_nFalseNegative);
-    
-    void LogMendelianIntersectionStatistic(int a_nChromosomeId,
-                                           int a_nCommonHomozygous,
-                                           int a_nCommonHeterozygous,
-                                           int a_nCommonFilteredHeterozygous,
-                                           int a_nFCHomozygous,
-                                           int a_nFCHeterozgous,
-                                           int a_nMCHomozygous,
-                                           int a_nMCHeterozygous,
-                                           int a_nChildUnique);
-    
     //Write the results in log.txt file
     void WriteStatistics();
-    
-    //Write the results of mendelian violation check in log.txt file
-    void WriteMendelianStatistics();
-    
+        
 private:
     
     //Log entry array for ga4gh output [THE STANTARD OUTPUT]
     SLogEntry m_aResultEntries[CHROMOSOME_COUNT];
-    
-    //Log entry array for Mendelian Violation mode output
-    SMendelianLogEntry m_aFatherChildLogEntries[CHROMOSOME_COUNT];
-    SMendelianLogEntry m_aMotherChildLogEntries[CHROMOSOME_COUNT];
-    SMendelianMergeLogEntry m_aChildMergeLogEntries[CHROMOSOME_COUNT];
-    
+        
     std::string m_aLogPath;
-    
 };
 
 
--- DuoComparison/src/CResultLog.cpp
+++ DuoComparison/src/CResultLog.cpp
@@ -28,43 +28,6 @@ void CResultLog::LogStatistic(int a_nChromosomeId, int a_nTpCalled, int a_nTpBas
     m_aResultEntries[a_nChromosomeId].m_bIsNull = false;
 }
 
-void CResultLog::LogMendelianStatistic(bool a_bIsFatherChild, int a_nChromosomeId, int a_nTpCalled, int a_nTpBaseline, int a_nFalsePositive, int a_nFalseNegative)
-{
-    SMendelianLogEntry* entry;
-    
-    if(true == a_bIsFatherChild)
-        entry = &m_aFatherChildLogEntries[a_nChromosomeId];
-    else
-        entry = &m_aMotherChildLogEntries[a_nChromosomeId];
-
-    entry->m_nTpBase = a_nTpBaseline;
-    entry->m_nTpCalled = a_nTpCalled;
-    entry->m_nFn = a_nFalseNegative;
-    entry->m_nFP = a_nFalsePositive;
-    entry->m_bIsNull = false;
-}
-
-void CResultLog::LogMendelianIntersectionStatistic(int a_nChromosomeId,
-                                                   int a_nCommonHomozygous,
-                                                   int a_nCommonHeterozygous,
-                                                   int a_nCommonFilteredHeterozygous,
-                                                   int a_nFCHomozygous,
-                                                   int a_nFCHeterozygous,
-                                                   int a_nMCHomozygous,
-                                                   int a_nMCHeterozygous,
-                                                   int a_nChildUnique)
-{
-    m_aChildMergeLogEntries[a_nChromosomeId].m_nFCHomozygous = a_nFCHomozygous;
-    m_aChildMergeLogEntries[a_nChromosomeId].m_nMCHomozygous = a_nMCHomozygous;
-    m_aChildMergeLogEntries[a_nChromosomeId].m_nFCHeterozygous = a_nFCHeterozygous;
-    m_aChildMergeLogEntries[a_nChromosomeId].m_nMCHeterozygous = a_nMCHeterozygous;
-    m_aChildMergeLogEntries[a_nChromosomeId].m_nCommonHomozygous = a_nCommonHomozygous;
-    m_aChildMergeLogEntries[a_nChromosomeId].m_nCommonHeterozygous = a_nCommonHeterozygous;
-    m_aChildMergeLogEntries[a_nChromosomeId].m_nChildUnique = a_nChildUnique;
-    m_aChildMergeLogEntries[a_nChromosomeId].m_nCommonFilteredHeterozygous = a_nCommonFilteredHeterozygous;
-    m_aChildMergeLogEntries[a_nChromosomeId].m_bIsNull = false;
-}
-
 //Write the results in log.txt file
 void CResultLog::WriteStatistics()
 {
@@ -153,93 +116,6 @@ void CResultLog::WriteStatistics()
     
     
     outputLog.close();
-}
-
-void CResultLog::WriteMendelianStatistics()
-{
-    std::ofstream outputLog;
-    outputLog.open(m_aLogPath);
-    
-    outputLog << "====== FATHER CHILD COMPARISON ======" << std::endl;
-    outputLog << "ID" << "\t" << "True-Pos-Child" << "\t\t" << "True-Pos-Father" << "\t\t" << "False-Pos" << "\t" << "False-Neg" << "\t" << "Precision" << "\t" << "Recall";
-    outputLog << "\t" << "F-measure" << std::endl;
-    
-    for(int k = 0; k < CHROMOSOME_COUNT; k++)
-    {
-        if(m_aFatherChildLogEntries[k].m_bIsNull)
-            continue;
-        
-        if(k < 22)
-            outputLog << "Chr" << k+1;
-        else if(k == 23)
-            outputLog << "ChrX";
-        
-        int TPbase = m_aFatherChildLogEntries[k].m_nTpBase;
-        int TPcalled = m_aFatherChildLogEntries[k].m_nTpCalled;
-        int FP = m_aFatherChildLogEntries[k].m_nFP;
-        int FN = m_aFatherChildLogEntries[k].m_nFn;
-        
-        outputLog << "\t" << TPcalled << "\t\t\t\t" << TPbase << "\t\t\t\t" << FP << "\t\t\t" << FN;
-        
-        double Precision = static_cast<double>(TPbase) / static_cast<double>(TPbase + FP);
-        double Recall = static_cast<double>(TPbase) / static_cast<double>(TPbase + FN);
-        double Fmeasure = Precision + Recall == 0.0 ? 0.0 : (2.0 * Precision * Recall) / (Precision + Recall);
-        
-        outputLog.precision(4);
-        outputLog << "\t" << Precision << "\t" << Recall << "\t" << Fmeasure << std::endl;
-    }
-    
-    outputLog << "====== MOTHER CHILD COMPARISON ======" << std::endl;
-    outputLog << "ID" << "\t" << "True-Pos-Child" << "\t\t" << "True-Pos-Mother" << "\t\t" << "False-Pos" << "\t" << "False-Neg" << "\t" << "Precision" << "\t" << "Recall";
-    outputLog << "\t" << "F-measure" << std::endl;
-    
-    for(int k = 0; k < CHROMOSOME_COUNT; k++)
-    {
-        if(m_aMotherChildLogEntries[k].m_bIsNull)
-            continue;
-        
-        if(k < 22)
-            outputLog << "Chr" << k+1;
-        else if(k == 23)
-            outputLog << "ChrX";
-        
-        int TPbase = m_aMotherChildLogEntries[k].m_nTpBase;
-        int TPcalled = m_aMotherChildLogEntries[k].m_nTpCalled;
-        int FP = m_aMotherChildLogEntries[k].m_nFP;
-        int FN = m_aMotherChildLogEntries[k].m_nFn;
-        
-        outputLog << "\t" << TPcalled << "\t\t\t\t" << TPbase << "\t\t\t\t" << FP << "\t\t\t" << FN;
-        
-        double Precision = static_cast<double>(TPbase) / static_cast<double>(TPbase + FP);
-        double Recall = static_cast<double>(TPbase) / static_cast<double>(TPbase + FN);
-        double Fmeasure = Precision + Recall == 0.0 ? 0.0 : (2.0 * Precision * Recall) / (Precision + Recall);
-        
-        outputLog.precision(4);
-        outputLog << "\t" << Precision << "\t" << Recall << "\t" << Fmeasure << std::endl;
-    }
-    
-    outputLog << std::endl << std::endl;
-    outputLog <<"======  MERGE STATISTICS ======" << std::endl;
-    outputLog << "Chr ID" << "\t" << "Common(Homozygous/Heterozygous/Filtered)" << "\t" << "Father-Side(Homozygous/Heterozygous)" << "\t" << "Mother-Side(Homozygous/Heterozygous)";
-    outputLog << "\t" << "Child-Unique" << std::endl;
-    
-    for(int k = 0; k < CHROMOSOME_COUNT; k++)
-    {
-        if(m_aChildMergeLogEntries[k].m_bIsNull)
-            continue;
-        
-        if(k < 22)
-            outputLog << "Chr" << k+1;
-        else if(k == 23)
-            outputLog << "ChrX";
-        
-        outputLog << "\t" << m_aChildMergeLogEntries[k].m_nCommonHomozygous   << " | " << m_aChildMergeLogEntries[k].m_nCommonHeterozygous << " | " << m_aChildMergeLogEntries[k].m_nCommonFilteredHeterozygous;
-        outputLog << "\t\t\t" << m_aChildMergeLogEntries[k].m_nFCHomozygous   << " | " << m_aChildMergeLogEntries[k].m_nFCHeterozygous;
-        outputLog << "\t\t\t\t" << m_aChildMergeLogEntries[k].m_nMCHomozygous << " | " << m_aChildMergeLogEntries[k].m_nMCHeterozygous;
-        outputLog << "\t\t\t\t" << m_aChildMergeLogEntries[k].m_nChildUnique;
-        outputLog << std::endl;
-    }
-    
 }
 
 
