a4442998fedb7f9a04d9df004d600d038e7186aa
Berke Toptas <berke.toptas@sbgenomics.com>
2017-08-08 17:49:38 -0400

    test.cpp is removed

--- main.cpp
+++ main.cpp
@@ -1,22 +1,6 @@
-#include <stdlib.h>
 #include <iostream>
-
-#include "CPathReplay.h"
-#include "SConfig.h"
-#include <ctime>
-#include <fstream>
-
 #include "CVcfAnalyzer.h"
 #include "CMendelianAnalyzer.h"
-#include <string>
-
-//void testGa4ghOutput();
-//void testRefOverlapOutput(int argc, char** argv);
-//void compareTrimmedVariables();
-//void testTripletReader2();
-//void compare2List(const std::string& s1, const std::string& s2);
-//void GenerateTruthSetsMaria(std::string a_rFilename, bool a_bIsFilterOverlap, bool a_bIsFilter00);
-//void UnitTestTrioComparison(int a_nChrNumber, bool a_bIsFilterOverlap, bool a_bIsFilter00);
 
 int main (int argc, char** argv)
 {
