002a11290054a1c3beb6d4e63f300163889a4003
Berke Toptas <berke.toptas@sbgenomics.com>
2017-11-12 23:29:14 -0500

    CVariantIterator cpp file is separated

--- MendelianViolation/include/CVariantIterator.h
+++ MendelianViolation/include/CVariantIterator.h
@@ -11,7 +11,11 @@
 #define _C_CALL_ITERATOR_H_
 
 #include <vector>
-#include "COrientedVariant.h"
+
+namespace core
+{
+    class COrientedVariant;
+}
 
 namespace mendelian
 {
@@ -24,49 +28,12 @@ class CVariantIterator
 {
 public:
     
-    CVariantIterator(const std::vector<const core::COrientedVariant*>& includedGT, const std::vector<const core::COrientedVariant*>& includedAM)
-    : m_aIncludedGT(includedGT),
-    m_aIncludedAM(includedAM)
-    {
-        it_IncludedGT = m_aIncludedGT.begin();
-        it_IncludedAM = m_aIncludedAM.begin();
-    }
+    CVariantIterator(const std::vector<const core::COrientedVariant*>& includedGT, const std::vector<const core::COrientedVariant*>& includedAM);
     
-    bool hasNext()
-    {
-        bool hasNext = (it_IncludedGT != m_aIncludedGT.end() || it_IncludedAM != m_aIncludedAM.end());
-        return hasNext;
-    }
+    bool hasNext();
     
-    const core::COrientedVariant* Next()
-    {
-        const core::COrientedVariant* toRet;
-        
-        if(it_IncludedAM == m_aIncludedAM.end())
-        {
-            toRet = *it_IncludedGT;
-            it_IncludedGT++;
-        }
-        
-        else if(it_IncludedGT != m_aIncludedGT.end() && (*it_IncludedGT)->GetVariant().GetOriginalPos() < (*it_IncludedAM)->GetVariant().GetOriginalPos())
-        {
-            toRet = *it_IncludedGT;
-            it_IncludedGT++;
-        }
-        
-        else if(it_IncludedGT != m_aIncludedGT.end() && (*it_IncludedGT)->GetVariant().GetOriginalPos() == (*it_IncludedAM)->GetVariant().GetOriginalPos())
-        {
-            toRet = (*it_IncludedGT)->GetVariant().m_nId < (*it_IncludedAM)->GetVariant().m_nId ? *(it_IncludedGT++) : *(it_IncludedAM++);
-        }
-        
-        else
-        {
-            toRet = *it_IncludedAM;
-            it_IncludedAM++;
-        }
-        
-        return toRet;
-    }
+    const core::COrientedVariant* Next();
+
     
 private:
     
---
+++ MendelianViolation/src/CVariantIterator.cpp
@@ -0,0 +1,56 @@
+//
+//  CVariantIterator.cpp
+//  VariantBenchmarkingTools
+//
+//  Created by Berke.Toptas on 11/12/17.
+//  Copyright © 2017 Seven Bridges Genomics. All rights reserved.
+//
+
+#include "CVariantIterator.h"
+#include "COrientedVariant.h"
+
+using namespace mendelian;
+
+CVariantIterator::CVariantIterator(const std::vector<const core::COrientedVariant*>& includedGT, const std::vector<const core::COrientedVariant*>& includedAM)
+: m_aIncludedGT(includedGT),
+m_aIncludedAM(includedAM)
+{
+    it_IncludedGT = m_aIncludedGT.begin();
+    it_IncludedAM = m_aIncludedAM.begin();
+}
+
+bool CVariantIterator::hasNext()
+{
+    bool hasNext = (it_IncludedGT != m_aIncludedGT.end() || it_IncludedAM != m_aIncludedAM.end());
+    return hasNext;
+}
+
+const core::COrientedVariant*  CVariantIterator::Next()
+{
+    const core::COrientedVariant* toRet;
+    
+    if(it_IncludedAM == m_aIncludedAM.end())
+    {
+        toRet = *it_IncludedGT;
+        it_IncludedGT++;
+    }
+    
+    else if(it_IncludedGT != m_aIncludedGT.end() && (*it_IncludedGT)->GetVariant().GetOriginalPos() < (*it_IncludedAM)->GetVariant().GetOriginalPos())
+    {
+        toRet = *it_IncludedGT;
+        it_IncludedGT++;
+    }
+    
+    else if(it_IncludedGT != m_aIncludedGT.end() && (*it_IncludedGT)->GetVariant().GetOriginalPos() == (*it_IncludedAM)->GetVariant().GetOriginalPos())
+    {
+        toRet = (*it_IncludedGT)->GetVariant().m_nId < (*it_IncludedAM)->GetVariant().m_nId ? *(it_IncludedGT++) : *(it_IncludedAM++);
+    }
+    
+    else
+    {
+        toRet = *it_IncludedAM;
+        it_IncludedAM++;
+    }
+    
+    return toRet;
+}
