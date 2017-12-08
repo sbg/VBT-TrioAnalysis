9bf3b2514253fa2eb5008c20e36025e541b44363
Berke Toptas <berke.toptas@sbgenomics.com>
2017-12-08 14:17:41 -0500

    Graph Comparison is removed from project main function header is modified

--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@
 #include <iostream>
 #include "CVcfAnalyzer.h"
 #include "CMendelianAnalyzer.h"
-#include "CGraphVcfAnalyzer.h"
 #include "Constants.h"
 
 int main (int argc, char** argv)
@@ -19,10 +18,11 @@ int main (int argc, char** argv)
     int successNo = 0;
     
     std::cout << "==== VARIANT BENCHMARKING TOOLS " << VBT_VERSION << " ==== " << std::endl;
-    std::cout << "Core Comparison engine is based on RTG paper: http://biorxiv.org/content/biorxiv/early/2015/08/02/023754.full.pdf" << std::endl;
     std::cout << "Author: Berke Cagkan Toptas (berke.toptas@sbgdinc.com)" << std::endl;
     std::cout << "Please send me an email if there is a problem (e.g. bug reporting, run-time errors)" << std::endl;
-    std::cout << "COPYRIGHT (C) 2017 SBGD INC" << std::endl;
+    std::cout << "COPYRIGHT (C) 2016 Seven Bridges Genomics" << std::endl;
+    std::cout << "          (C) 2017 SBGD Inc" << std::endl;
+    std::cout << "All rights reserved." << std::endl;
     std::cout << std::endl;
     
     if(argc == 1)
@@ -30,9 +30,7 @@ int main (int argc, char** argv)
         std::cerr << "You have not entered an input.Please try either following:" << std::endl;
         std::cerr << "./vbt varcomp [PARAMETERS]" << std::endl;
         std::cerr << "./vbt mendelian [PARAMETERS]" << std::endl;
-        std::cerr << "./vbt graphcomp [PARAMETERS]" << std::endl;
         std::cerr << "Please type ./vbt <select_feature> --help for short info about the parameter structure." << std::endl;
-        
         return -1;
     }
     
@@ -47,19 +45,12 @@ int main (int argc, char** argv)
        duocomparison::CVcfAnalyzer analyzer;
        analyzer.Run(argc, argv);
     }
-
-    else if(strcmp(argv[1], "graphcomp") == 0)
-    {
-        graphcomparison::CGraphVcfAnalyzer analyzer;
-        successNo = analyzer.run(argc, argv);
-    }
     
     else
     {
         std::cerr << "Invalid feature name.Please try either following:" << std::endl;
         std::cerr << "./vbt varcomp [PARAMETERS]" << std::endl;
         std::cerr << "./vbt mendelian [PARAMETERS]" << std::endl;
-        std::cerr << "./vbt graphcomp [PARAMETERS]" << std::endl;
         std::cerr << "Please type ./vbt <select_feature> --help for short info about the parameter structure." << std::endl;
         return -1;
     }
