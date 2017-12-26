84109846057a86c867428572ddce128fa155ec6c
Berke Toptas <berke.toptas@sbgenomics.com>
2017-12-26 18:26:24 -0500

    Version is updated to 0.7.5

--- main.cpp
+++ main.cpp
@@ -20,9 +20,8 @@ int main (int argc, char** argv)
     std::cout << "==== VARIANT BENCHMARKING TOOLS " << VBT_VERSION << " ==== " << std::endl;
     std::cout << "Author: Berke Cagkan Toptas (berke.toptas@sbgdinc.com)" << std::endl;
     std::cout << "Please send me an email if there is a problem (e.g. bug reporting, run-time errors)" << std::endl;
-    std::cout << "COPYRIGHT (C) 2016 Seven Bridges Genomics" << std::endl;
-    std::cout << "          (C) 2017 SBGD Inc" << std::endl;
-    std::cout << "All rights reserved." << std::endl;
+    std::cout << "Copyright 2017 Seven Bridges Genomics Inc." << std::endl;
+    std::cout << std::endl;
     std::cout << std::endl;
     
     if(argc == 1)
@@ -57,5 +56,3 @@ int main (int argc, char** argv)
     
     return successNo;
 }
-
-
